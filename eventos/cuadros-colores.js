3219ad0ac6b23619e24b1dae143a1dfaf254af02
Carlos Colorado <caancoqu@gmail.com>
2024-04-19 17:58:46 -0500

    Aprendiendo sobre manejo de eventos

--- eventos/cuadros-colores.js
+++ eventos/cuadros-colores.js
@@ -11,5 +11,11 @@ function random(number) {
   
   container.addEventListener(
     "click",
-    (event) => (event.target.style.backgroundColor = bgChange()),
-  );
+    (event) => {event.target.style.backgroundColor = bgChange()});
+
+    /*
+    En el evento click que has añadido al contenedor (#container), event.target se refiere al elemento específico que ha sido el objetivo del evento, es decir, al elemento que ha sido clickeado dentro del contenedor.
+    
+    event es el objeto que representa el evento de clic. Cuando se produce un clic dentro del contenedor, este evento es activado. event.target se refiere al elemento que ha sido clickeado dentro del contenedor. Entonces, event.target.style.backgroundColor establece el color de fondo del elemento clickeado en el color generado por la función bgChange().
+
+    Este enfoque es útil porque permite que la función bgChange() sea aplicada solo al elemento específico que ha sido clickeado, en lugar de cambiar el color de fondo del contenedor en su totalidad. Esto hace que la interacción sea más precisa y específica para el usuario.*/
