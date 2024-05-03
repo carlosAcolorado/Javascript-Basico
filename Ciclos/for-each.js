2775210c7b819db4910bf127abb4a8321ea5da92
Carlos Colorado <caancoqu@gmail.com>
2024-05-03 11:04:35 -0500

    Creando un stack de libros con html, css y javascript

--- Ciclos/for-each.js
+++ Ciclos/for-each.js
@@ -2,6 +2,8 @@
 array.foreach((item)=>{
     codigo a ejecutar
 })
+
+sintaxis:  array.forEach(function(elementoActual, indice, array))
 */
 
 let list = ["eat","sleep","code","repeat"]
@@ -11,3 +13,8 @@ list.forEach((item)=>{
 })//es un metodo mas sencillo para recorrer listas
 
 
+const vocales = ["a", "e", "i", "o", "u"];
+
+vocales.forEach(function (vocal, indice, arr) {
+  console.log(`El valor del elemento en la posición ${indice} es ${vocal} del array que estamos recorriendo que es ${arr}`);
+});
--- Stack-de-libros/stack-libros.js
+++ Stack-de-libros/stack-libros.js
@@ -1,17 +1,64 @@
  let bookCart = []
-
+let ul = document.querySelector('.miLista')
+ let input= document.querySelector('.entrada');
+ let boton = document.querySelector('.boton')
 const ADD_TO_CART_ACTION = 'addToCart'
 const REMOVE_FROM_CART_ACTION = 'removeFromCart'
 const VIEW_CART_ACTION = 'removeFromCart'
 
-function viewCart(){
-    console.log('Current Cart of Books: ',bookCart)
+/*function viewCart(){
+    ul.textContent = ''
+    bookCart.forEach((item)=>{
+        let li = document.createElement("li");
+        li.textContent = item;
+        ul.appendChild(li);
+})
+    }*/
+
+    function viewCart(){
+        // Limpiar el contenido actual de la lista
+        ul.innerHTML = '';
+    
+        // Mostrar las tareas nuevamente sin repetir las anteriores
+        bookCart.forEach((item, index) => {
+            let li = document.createElement("li");
+            let imagen = document.createElement("img")
+            imagen.src = "./book-3_icon-icons.com_49257.png"
+            li.classList.add('libros');
+            li.textContent = item;
+            
+    
+            // Crear un botón de eliminación
+            let deleteButton = document.createElement("button");
+            deleteButton.textContent = "Eliminar";
+            deleteButton.addEventListener('click', function() {
+                // Eliminar la tarea correspondiente del array bookCart
+                bookCart.splice(index, 1);
+                // Volver a mostrar la lista actualizada
+                viewCart();
+            });
+    
+            // Agregar el botón de eliminación al elemento de lista
+            li.appendChild(deleteButton);
+            li.appendChild(imagen)
+    
+            // Agregar el elemento de lista a la lista ul
+            ul.appendChild(li);
+        });
+    }
+
+function agregar(){
+    bookCart.push(input.value)
+    viewCart()
+    input.value = ''
 }
 
-function performCartActions (action,newBook){
+boton.addEventListener('click',agregar)
+
+/*function performCartActions (action,newBook){
     switch (action){
         case ADD_TO_CART_ACTION :
-            bookCart.push(newBook)
+            
             break;
         case REMOVE_FROM_CART_ACTION :
             if (bookCart.length === 0){
@@ -31,7 +78,7 @@ function performCartActions (action,newBook){
            
     }
 }
+*/  
 
-performCartActions(ADD_TO_CART_ACTION,'El tunel')
-performCartActions(ADD_TO_CART_ACTION,'Historias perdidas')
-performCartActions(viewCart())
+
+
